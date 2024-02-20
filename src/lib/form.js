d0d033b555fc4b3c00bb507e149029bcf1e059a5
Ritikreacts <hrithikms22@gmail.com>
2024-02-20 15:07:55 +0530

    first commit

--- src/lib/form.js
+++ src/lib/form.js
@@ -1,9 +1,16 @@
-export default class Form {
-  constructor(formContainerId, formData) {
-    this.container = document.getElementById(formContainerId); //Container element from HTML in which you have to add form
-    // Pass formContainerId to append form element inside of HTML DIV element
-    // use formData to create form
-    console.log('Form', formData);
+export default class Form{
+  constructor(formContainerId, formData){
+   this.container = document.getElementById(formContainerId);
+   this.formData = formData;
+   this.form = document.createElement('form');
+   this.createForm()
+
   }
-  // create methods/event to create form/ reset form/ submit form, etc
-}
+  createForm(){
+   this.formData.foreach(field=>{
+    //create input field
+     this.form.appendChild(inputElement);
+
+   })
+  }
+ }
